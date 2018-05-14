14819328e1b6cbd52a881bcaa247197d458fb762
sevenflash <sevenflash@yandex.ru>
2018-05-14 13:47:18 +0300

    bonus distributor

--- contracts/BonusDistributor.sol
+++ contracts/BonusDistributor.sol
@@ -1,3 +1,37 @@
 pragma solidity ^0.4.23;
 
-contract BonusDistributor {}
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import "./BonusToken.sol";
+
+contract BonusDistributor is Ownable {
+    BonusToken token;
+
+    uint256 TOKEN = 10 ** 18;
+
+    constructor(address _token) {
+        token = BonusToken(_token);
+    }
+
+    function payout(address _beneficiary, uint256 _paymentAmount)
+        external onlyOwner
+    {
+        uint256 bonusAmount = calculateBonus(_paymentAmount);
+
+        token.transfer(_beneficiary, bonusAmount);
+    }
+
+    function calculateBonus(uint256 _amount)
+        public constant returns(uint256)
+    {
+        if(_amount == 0) return 0;
+
+        if(_amount < 5 * TOKEN / 10)
+            return 1 * TOKEN;
+
+        if(_amount >= 5 * TOKEN / 10 && _amount <= 1 * TOKEN)
+            return 10 * TOKEN;
+
+        return _amount / 100;
+    }
+}
--- test/BonusDistributor.js
+++ test/BonusDistributor.js
@@ -10,7 +10,7 @@ contract('BonusDistributor', function([creator, client, secondClient, thirdClien
 	before(async function() {
 		this.token = await BonusToken.new({ from: creator });
 
-		this.distributor = await BonusDistributor.new({ from: creator });
+		this.distributor = await BonusDistributor.new(this.token.address, { from: creator });
 
 		await this.token.transfer(
 			this.distributor.address,
@@ -30,18 +30,24 @@ contract('BonusDistributor', function([creator, client, secondClient, thirdClien
 	it('should fail to pay more bonuses than balance of the contract', async function() {
 		const balance = await this.token.balanceOf(this.distributor.address);
 
-		await expectThrow(this.distributor.payout(client, balance.mul(100).add(1), { from: creator }));
+		await expectThrow(this.distributor.payout(client, balance.add(1).mul(100), { from: creator }));
 	});
 
 	it('should pay 1 bonus token for payments with amount less than 0.5 token', async function() {
-		await this.distributor.payout(client, 1, { from: creator });
+		await this.distributor.payout(secondClient, 1, { from: creator });
 
-		expect(await this.token.balanceOf(client)).to.be.bignumber.equal(units(1));
+		expect(await this.token.balanceOf(secondClient)).to.be.bignumber.equal(units(1));
 	});
 
-	it('should pay 10 bonus tokens for payment with amount greater than 0.5 token', async function() {
-		await this.distributor.payout(secondClient, units(0.5), { from: creator });
+	it('should pay 10 bonus tokens for payment with amount greater than 0.5 token and less than 1 token', async function() {
+		await this.distributor.payout(thirdClient, units(0.5), { from: creator });
 
-		expect(await this.token.balanceOf(secondClient)).to.be.bignumber.equal(units(10));
+		expect(await this.token.balanceOf(thirdClient)).to.be.bignumber.equal(units(10));
+	});
+
+	it('should pay 0.11 bonus tokens for payment with 1.1 amount', async function() {
+		await this.distributor.payout(client, units(1.1), { from: creator });
+
+		expect(await this.token.balanceOf(client)).to.be.bignumber.equal(units(0.011));
 	});
 });
